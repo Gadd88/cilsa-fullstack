c0f27a1f3846033a2947e5fbf32af7ab357d3bcc
Matias Saade <matiassaade@gmail.com>
2024-07-01 16:43:55 -0300

    add: tarea formulario simple y ejercicios css

---
+++ ejercicios_formularios_css/formulario.js
@@ -0,0 +1,25 @@
+const form = document.getElementById('form')
+const btnSubmit = document.getElementById('btn-submit')
+const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault()
+    const formData = new FormData(form)
+    const data = Object.fromEntries(formData.entries())
+    if (!emailRegex.test(data.email)) {
+        alert('Email invalido')
+        return
+    }
+    if(data.name === '' || data.lastName === '' || data.email === '' || data.birthDate === '' || data.country === '') {
+        alert('Todos los campos son requeridos')
+        return
+    }
+    alert('Formulario Enviado')
+    form.reset()
+})
+
+const btnContrast = document.getElementById('btn-contrast')
+
+btnContrast.addEventListener('click', () => {
+    document.body.classList.toggle('contrast')
+})
